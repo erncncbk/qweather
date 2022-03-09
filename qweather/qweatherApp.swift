abed95b7fde5ba4b16666d9d5377452e2cb2917b
Erencan Çabuk <erncncbk@gmail.com>
2022-03-10 01:31:50 +0300

    Initial Commit

---
+++ qweather/qweatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  qweatherApp.swift
+//  qweather
+//
+//  Created by Erencan on 10.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct qweatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
